1e2db15e1e572a20faa193d566c8a5d117718189
mikeblackett <mike.blackett13@gmail.com>
2022-11-14 19:49:23 +0000

    data preparation for db

--- python/utilities/netcdf.py
+++ python/utilities/netcdf.py
@@ -16,32 +16,3 @@ def list_files(directory, recursive=False, path=False):
                 else:
                     nc_files.append(file)
     return nc_files
-
-
-names = {
-    "groundfrost": "Frost",
-    "sun": "Sun",
-    "tas": "Temperature",
-    "snowLying": "Snow",
-    "tasmax": "Maximum temperature",
-    "hurs": "Humidity",
-    "rainfall": "Rain",
-    "sfcWind": "Wind",
-    "tasmin": "Minimum temperature",
-}
-
-
-def get_attributes(data_array, to_drop=None):
-    """
-    Returns a dict describing the attributes of a DataArray
-    Optionally, filters the attributes with to_drop
-    """
-    assert isinstance(to_drop, list) or to_drop is None
-    assert isinstance(data_array, xr.DataArray)
-    attrs_dict = {}
-    for key, value in data_array.attrs.items():
-        if key not in to_drop:
-            attrs_dict[key] = value
-    attrs_dict["code"] = data_array.name
-    attrs_dict["name"] = names[attrs_dict["code"]]
-    return attrs_dict
