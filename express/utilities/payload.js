8af95ac06bdcca468c63c23acef8221ca956ffd8
mikeblackett <mike.blackett13@gmail.com>
2022-11-23 14:37:57 +0000

    use class for payload

--- express/utilities/payload.js
+++ express/utilities/payload.js
@@ -1,42 +1,59 @@
+/**
+ * JSend response types
+ * @readonly
+ * @enum {string}
+ */
 const status = {
   SUCCESS: 'success',
   ERROR: 'error',
   FAIL: 'fail',
 }
 
-// getStatus(code) {
-//   if (code >= 500) {
-//     return status.ERROR
-//   }
-//   if (code >= 400) {
-//     return status.FAIL
-//   }
-//   return status.SUCCESS
-// }
+/**
+ * Converts an HTTP status code to a JSend response type
+ * @param {number} code An HTTP status code
+ * @returns {string} A JSend response type
+ */
+function getStatus(code) {
+  if (code >= 500) {
+    return status.ERROR
+  }
+  if (code >= 400) {
+    return status.FAIL
+  }
+  return status.SUCCESS
+}
 
-const payload = {
-  success(data = {}) {
+/**
+ * A JSend-compliant server response object
+ * @see {@link https://github.com/omniti-labs/jsend}
+ */
+class Payload {
+  static getStatus(statusCode) {
+    return getStatus(statusCode)
+  }
+
+  success(data) {
     return {
       status: status.SUCCESS,
       data,
     }
-  },
+  }
 
-  fail(message = '') {
+  fail(data) {
     return {
       status: status.FAIL,
-      data: {
-        message,
-      },
+      data,
     }
-  },
+  }
 
-  error(message = '') {
+  error(message, code) {
     return {
       status: status.ERROR,
+      code,
       message,
     }
-  },
+  }
 }
 
-export { payload }
+export { Payload }
