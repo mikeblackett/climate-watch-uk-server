71a37e1be4520b73cd95c151323bd990feb0eab5
mikeblackett <mike.blackett13@gmail.com>
2022-11-23 14:37:19 +0000

    add jsdoc

--- express/utilities/error.js
+++ express/utilities/error.js
@@ -1,3 +1,6 @@
+/**
+ * Custom error class which supports an HTTP status code as a property
+ */
 class ApiError extends Error {
   constructor(message, { cause, status }) {
     super(message, { cause })
