1c9d8ef6ba954915671f777a21d7baac2f8989eb
mikeblackett <mike.blackett13@gmail.com>
2022-11-24 14:35:59 +0000

    add client/server error classes

---
+++ express/error/client.error.js
@@ -0,0 +1,47 @@
+import { ApiError } from './base.error.js'
+
+/**
+ * @enum Types of client error HTTP status codes
+ */
+const statusCodes = {
+  ClientError: 400,
+  BadRequestError: 400,
+  UnauthorizedError: 401,
+  ForbiddenError: 403,
+  NotFoundError: 404,
+  MethodNotAllowedError: 404,
+}
+
+/**
+ * Error class representing client error responses
+ * @extends ApiError
+ */
+class ClientError extends ApiError {
+  constructor(message, { cause, statusCode = 400 } = {}) {
+    super(message, { cause })
+    this.statusCode = statusCodes[this.name] ?? statusCode
+  }
+}
+
+/**
+ * 400 Bad Request error
+ * @extends ClientError
+ */
+class BadRequestError extends ClientError {
+  constructor(request, { cause }) {
+    super(`${request} malformed!`, { cause })
+  }
+}
+
+/**
+ * 404 Not Found error
+ * @extends ApiError
+ */
+class NotFoundError extends ClientError {
+  /** @inheritdoc */
+  constructor(resource, { cause }) {
+    super(`${resource} not found!`, { cause })
+  }
+}
+
+export { ClientError, NotFoundError, BadRequestError }
