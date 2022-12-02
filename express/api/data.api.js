07fe4f52471881e5ba5affd8b84eae45a8176f71
mikeblackett <mike.blackett13@gmail.com>
2022-12-02 19:58:35 +0000

    add seasonal summary query

--- express/api/data.api.js
+++ express/api/data.api.js
@@ -34,6 +34,31 @@ function annualAverages(location) {
     .where('o.location_id', location)
 }
 
+const seasonsEnum = {
+  win: [12, 1, 2],
+  spr: [3, 4, 5],
+  sum: [6, 7, 8],
+  aut: [9, 10, 11],
+}
+
+function seasonalAverages(location, season) {
+  return Observation.query()
+    .alias('o')
+    .select(
+      'o.variable_id',
+      'o.location_id',
+      't.season_year as year',
+      raw(
+        `dense_rank() over (partition by o.variable_id, o.location_id order by avg(o.value) desc)`
+      ).as('rank'),
+      raw(`round(avg(o.value), 1)`).as('value')
+    )
+    .joinRelated('times', { alias: 't' })
+    .groupByRaw('t.season_year, o.variable_id, o.location_id')
+    .where('o.location_id', location)
+    .whereIn('t.month_number', seasonsEnum[season])
+}
+
 function monthlyValues(location, month_number) {
   return Observation.query()
     .alias('o')
@@ -62,7 +87,7 @@ function countYears(location) {
 }
 
 async function getClimateSummary(request, response, next) {
-  const { location, month, variable, year } = request.query
+  const { location, month, season, variable, year } = request.query
   try {
     if (!location || !year) {
       throw new BadRequestError(`Missing required query parameters`, {
@@ -84,6 +109,8 @@ async function getClimateSummary(request, response, next) {
       query = query
         .select('o.month_number')
         .from(monthlyValues(location, month).as('o'))
+    } else if (season) {
+      query = query.from(seasonalAverages(location, season).as('o'))
     } else {
       query = query.from(annualAverages(location).as('o'))
     }
