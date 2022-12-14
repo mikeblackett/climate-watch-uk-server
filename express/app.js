97266b0479b1bc5f61a902b18e20c6c406524828
mikeblackett <mike.blackett13@gmail.com>
2022-12-14 17:16:29 +0000

    update jsend-compliant  payload handler

--- express/app.js
+++ express/app.js
@@ -3,17 +3,14 @@
 import express from 'express'
 import cors from 'cors'
 import { addRoutes } from './routes/routes.js'
-import { Payload } from './api/utilities/Payload.js'
-import { ClientError } from './error/client.error.js'
+import jsend from './utilities/jsend.js'
+import { ClientError } from './utilities/error/client.error.js'
 
 const app = express()
-const payload = new Payload()
 const corsOptions = {
   origin: 'http://localhost:8081',
 }
 app.use(cors(corsOptions))
-
-// app.use(logger('dev'))
 
 app.set('json replacer', function (key, value) {
   if (this[key] instanceof Date) {
@@ -39,17 +36,18 @@ addRoutes(app)
 
 // Catch any errors...
 app.use((error, request, response, next) => {
+  console.log('error handled')
   response.status(error.statusCode || 500)
   if (error instanceof ClientError) {
-    return response.json(payload.fail(error.cause))
+    return response.json(jsend.fail(error.cause))
   }
-  response.json(payload.error(error.message, response.statusCode))
+  response.json(jsend.error(error.message, response.statusCode))
 })
 
 // Finally, unhandled requests fail with a 404 not found response
 app.use((request, response) => {
   response.status(404)
-  response.json(payload.fail({ resource: 'Not found!' }))
+  response.json(jsend.fail({ resource: 'Not found!' }))
 })
 
 process.on('uncaughtException', (error) => {
---
+++ express/utilities/jsend.js
@@ -0,0 +1,54 @@
+/**
+ * @module jsend A JSend-compliant server responses
+ * @see {@link https://github.com/omniti-labs/jsend}
+ */
+
+/**
+ * JSend response types
+ * @readonly
+ * @enum {string}
+ */
+const status = {
+  SUCCESS: 'success',
+  ERROR: 'error',
+  FAIL: 'fail',
+}
+
+/**
+ * Converts an HTTP status code to a JSend response type
+ * @param {number} code An HTTP status code
+ * @returns {string} A JSend response type
+ */
+function getStatus(code) {
+  if (code >= 500) {
+    return status.ERROR
+  }
+  if (code >= 400) {
+    return status.FAIL
+  }
+  return status.SUCCESS
+}
+
+function success(data) {
+  return {
+    status: status.SUCCESS,
+    data,
+  }
+}
+
+function fail(data) {
+  return {
+    status: status.FAIL,
+    data,
+  }
+}
+
+function error(message, code) {
+  return {
+    status: status.ERROR,
+    code,
+    message,
+  }
+}
+
+export default { success, fail, error, getStatus }
