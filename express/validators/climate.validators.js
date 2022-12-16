5732bafc2bd482f27392e739e122d66c6c0fd74f
mikeblackett <mike.blackett13@gmail.com>
2022-12-16 18:41:22 +0000

    reuseable schema properties

--- express/validators/climate.validators.js
+++ express/validators/climate.validators.js
@@ -2,25 +2,32 @@ import validate from '../middlewares/json-validator.middleware.js'
 import { variableIds } from './variable.validators.js'
 import { locationIds } from './location.validators.js'
 
+const location = {
+        type: 'string',
+        enum: locationIds,
+}
+const year = {
+        type: 'string',
+        pattern: '^(183[6-9]|18[4-9][0-9]|19[0-9][0-9]|20[0-1][0-9]|202[0-1])$',
+}
+const variable = {
+        type: 'string',
+        enum: variableIds,
+}
+const month = {
+        type: 'string',
+        pattern: '^([1-9]|1[0-2]{1})$',
+}
 const getSnapshot = validate({
   query: {
     type: 'object',
     properties: {
-      location: {
-        type: 'string',
-        enum: locationIds,
-      },
-      year: {
-        type: 'string',
-        pattern: '^(183[6-9]|18[4-9][0-9]|19[0-9][0-9]|20[0-1][0-9]|202[0-1])$',
-      },
-      variable: {
-        type: 'string',
-        enum: variableIds,
-      },
-      month: {
-        type: 'string',
-        pattern: '^([1-9]|1[0-2]{1})$',
+      location,
+      year,
+      variable,
+      month,
+    },
+    required: ['location', 'year'],
       },
     },
     required: ['location', 'year'],
