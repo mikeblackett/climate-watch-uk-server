54c0d855a4f96743d79d8a283acc3fb9df31582b
mikeblackett <mike.blackett13@gmail.com>
2022-12-16 13:49:32 +0000

    update validation

--- express/validators/climate.validators.js
+++ express/validators/climate.validators.js
@@ -1,4 +1,11 @@
 import validate from '../middlewares/json-validator.middleware.js'
+import { getAllIds } from '../services/variable.services.js'
+
+const variables = await getAllIds()
+const variableEnum = variables.reduce((p, c) => {
+  p.push(c.id)
+  return p
+}, [])
 
 const getSnapshot = validate({
   query: {
@@ -9,12 +16,15 @@ const getSnapshot = validate({
       },
       year: {
         type: 'string',
+        pattern: '^(183[6-9]|18[4-9][0-9]|19[0-9][0-9]|20[0-1][0-9]|202[0-1])$',
       },
       variable: {
         type: 'string',
+        enum: variableEnum,
       },
       month: {
         type: 'string',
+        pattern: '^([1-9]|1[0-2]{1})$',
       },
     },
     required: ['location', 'year'],
